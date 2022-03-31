c0ca3da0b7f62604e9fe81bdbeb151bcc3fa0767
Martijns3 <spierenburg2480@gmail.com>
2022-03-31 13:48:47 +0200

    functions

---
+++ Documents/Winc/functions/main.py
@@ -0,0 +1,26 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+def greet(name):    
+    #return('Hello, ' + name + '!')
+    return f'Hello, {name}!'
+
+#greet("Bob")
+
+def add(x,y,z):
+    return(x + y + z)
+    #print(sum)
+
+add(5,3,2)
+
+def positive(x):
+    return(x > 0)
+    
+#positive(5)
+
+def negative(x):
+    return(x < 0)
+    
+#negative(0)
