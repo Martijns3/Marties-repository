3c55de895aacfd5ee60350ffa96db03b61e75996
Martijns3 <spierenburg2480@gmail.com>
2022-10-25 17:38:10 -0400

    print

---
+++ print/main.py
@@ -0,0 +1,17 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+import numbers
+from tokenize import Number
+
+
+print("WINCPY is crazyyy!!!")
+print("Je suis une crepemaison!!")
+print("Hallo hoe heet ik ?")
+
+Number = 3
+name = "adri"
+print(Number)
+type (Number)
