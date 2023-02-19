c732bb5e65992d5edd08cd9b9e5b8d71c47a9ed8
Martijns3 <spierenburg2480@gmail.com>
2023-02-19 11:26:23 +0100

    testing

--- testing/main.py
+++ testing/main.py
@@ -1,16 +1,7 @@
 def get_none():
     return None
 
-
-# def flatten_dict(dict):
-#     list = [*dict.values()]
-#     return list
-
-# print(flatten_dict(d))
-
 final_list = []
-
-
 def flatten_dict(d):
 
     for k, v in d.items():
@@ -20,19 +11,24 @@ def flatten_dict(d):
 
         elif type(v) == list:
             for i in v:
-                flatten_dict(i)
+                if type(i)!=dict:
+                    final_list.append(i)
+                else:
+                    flatten_dict(i)
+
         else:
             final_list.append(v)
 
     return final_list
 
+# dictA = {"a": {"inner_a": {"inner_inner_f": 34}, "inner_b": 350}, "b": 3.14}
+# dictB = {"a": {"inner_a": 42, "inner_b": 350}, "b": {3.14: "bleh"}}
+# dictC = {"a": [({"inner_inner_a": 42})]}
+# dictD = {'a': [42, 350], 'b': 3.14}
+
+# print(flatten_dict(dictC))
+
+
     # 1. Als het een dictionary is, ga dan door elke key heen en roep voor elke value flatten_dict weer aan. Voeg de waarde die je terugkrijgt aan final_list toe
     # 2. Als het een list is, ga door elk element van de lijst heen en roep voor elk element flatten_dict weer aan. Voeg de waarde die je terugkrijgt aan final_list toe
     # 3. Geen van beiden (else) dan zit je helemaal onderin je dictionary dus die voeg je dan sowieso to aan final_list (final_list += [in_dict])
-
-
-dictA = {"a": {"inner_a": {"inner_inner_f": 34}, "inner_b": 350}, "b": 3.14}
-dictB = {"a": {"inner_a": 42, "inner_b": 350}, "b": {3.14: "bleh"}}
-dictC = {"a": [{"inner_inner_a": 42}]}
-
-print(flatten_dict(dictC))
--- testing/test_main.py
+++ testing/test_main.py
@@ -1,17 +1,22 @@
 from main import get_none
 from main import flatten_dict
 
+# from main import test_3
 
 def test_get_none():
     assert get_none() == None
 
 
+# def test_flatten_dict():
+#     assert (
+#         type(
+#             flatten_dict(
+#                 {"a": [{"inner_inner_a": 42}]}
+#             )
+#         )
+#         == list
+#     )
+
+
 def test_flatten_dict():
-    assert (
-        type(
-            flatten_dict(
-                {"a": [{"inner_inner_a": 42}]}
-            )
-        )
-        == list
-    )
+    assert flatten_dict({"a": {"inner_a": {"inner_inner_f": 34}, "inner_b": 350}, "b": 3.14}) ==[34, 350, 3.14]
